387499c58fff96d952c90d56aeda3c504ba93018
livexce <ayoub.elmazouzi@livecampus.tech>
2025-05-16 11:35:48 +0200

    Fix flake8 : éviter import spacy + ordre des imports

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,15 +1,9 @@
 import pytest
 import pandas as pd
+import importlib.util
 
-# Skip ce fichier dans la CI si spaCy n’est pas dispo
-spacy_available = False
-try:
-    import spacy
-
-    spacy_available = True
-except ImportError:
-    pass
-
+# Vérifie si spaCy est installée
+spacy_available = importlib.util.find_spec("spacy") is not None
 pytestmark = pytest.mark.skipif(not spacy_available, reason="spaCy not available in CI")
 
 from scripts.preprocess import drop_duplicates_and_na
