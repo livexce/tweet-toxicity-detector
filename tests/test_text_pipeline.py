151ae0cd7ded111b9583637243fbb52775a07191
livexce <ayoub.elmazouzi@livecampus.tech>
2025-05-16 11:53:23 +0200

    Skip test_text_pipeline.py if spaCy is not installed

--- tests/test_text_pipeline.py
+++ tests/test_text_pipeline.py
@@ -1,7 +1,12 @@
+import pytest
+import importlib.util
 from scripts.preprocess import build_text_pipeline
 
 
 def test_text_pipeline_outputs_matrix():
+    if importlib.util.find_spec("spacy") is None:
+        pytest.skip("spaCy not available in CI")
+
     pipe = build_text_pipeline()
     X = pipe.fit_transform(["I love pizza", "I hate spam"])
     assert hasattr(X, "toarray")
